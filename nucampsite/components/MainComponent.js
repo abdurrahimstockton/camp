8a86c4da77ba79a799eb50ee20bea7e4252f1add
abdur <abdur@abdurs-MacBook-Pro.local>
2020-07-20 22:14:30 -0400

    Login Component added, Main Component and Reservation Component updated

--- nucampsite/components/MainComponent.js
+++ nucampsite/components/MainComponent.js
@@ -6,6 +6,7 @@ import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoritesComponent';
+import Login from './LoginComponent';
 
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems } from 'react-navigation';
@@ -181,6 +182,30 @@ const FavoritesNavigator = createStackNavigator(
     }
 );
 
+const LoginNavigator = createStackNavigator(
+    {
+        Login: { screen: Login }
+    },
+    {
+        navigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='sign-in'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
+
+
 
 
 
@@ -207,7 +232,21 @@ const CustomDrawerContentComponent = props => (
 
 
 const MainNavigator = createDrawerNavigator(
-    {
+    {Login: {
+        screen: LoginNavigator,
+        navigationOptions: {
+            drawerIcon: ({tintColor}) => (
+                <Icon
+                    name='sign-in'
+                    type='font-awesome'
+                    size={24}
+                    color={tintColor}
+                />
+            )
+        }
+    },
+
+   
         Home: { screen: HomeNavigator, 
             navigationOptions: {
                 drawerIcon: ({tintColor}) => (
@@ -302,7 +341,8 @@ const MainNavigator = createDrawerNavigator(
 
 
 
-    {
+    {   
+        initialRouteName: 'Home',
         drawerBackgroundColor: '#CEC8FF', 
         contentComponent: CustomDrawerContentComponent
     }
